28b3157288575e6eabbe8cdcfa5553878dfbbb21
IndigoJX <12233980@qq.com>
2024-12-24 10:02:58 +0800

    Initial commit

---
+++ html/Indigo.github.io-main/script_pertubation.js
@@ -0,0 +1,16 @@
+//js
+
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click", function() {
+    var char = document.getElementById("data").value;
+    var box=document.querySelector(".box");
+    var img = document.createElement("img");
+
+    box.innerHTML = '';
+
+    img.src = "imagesjpg_pertubation/" + char + ".jpg";
+	img.height = 300;
+    box.appendChild(img);
+    console.log(img.src);
+})
